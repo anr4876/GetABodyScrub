d028498aa88fb84067626166e2ee194f08da81d0
Motoong <sonada903@gmail.com>
2023-08-07 10:35:45 +0900

    start

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start wor</Text>
+    
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
