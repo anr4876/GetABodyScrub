4878cbb229893991e250de0121043af941232806
anr4876 <dustjdanr@naver.com>
2023-08-08 14:40:46 +0900

    상품 선택 스크린

--- src/screens/menu1/createPost.js
+++ src/screens/menu1/createPost.js
@@ -1,15 +1,18 @@
 import * as React from "react";
-import { Button, TextInput } from "react-native";
+import { Button, TextInput, StyleSheet, Dimensions, View } from "react-native";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 export default function CreatePostScreen({ navigation }) {
   const [postText, setPostText] = React.useState("");
 
   return (
-    <>
+    <View style={styles.container}>
       <TextInput
         multiline
         placeholder="What's on your mind?"
-        style={{ height: 200, padding: 10, backgroundColor: "white" }}
+        style={styles.input}
         value={postText}
         onChangeText={setPostText}
       />
@@ -24,6 +27,36 @@ export default function CreatePostScreen({ navigation }) {
           });
         }}
       />
-    </>
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    padding: 10,
+    backgroundColor: "white",
+    height: windowHeight * 0.045,
+    width: windowWidth * 0.6,
+    alignSelf: "center",
+    marginBottom: 20,
+    borderColor: "#DD5151",
+    borderWidth: 2,
+    borderRadius: 8,
+    // iOS 스타일 속성
+    ...Platform.select({
+      ios: {
+        mixBlendMode: "color-burn",
+        backdropFilter: "blur(2px)",
+      },
+      // Android 스타일 속성
+      android: {
+        elevation: 5,
+      },
+    }),
+  },
+});
