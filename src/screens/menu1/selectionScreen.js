d4a98651da6b2b5d54d023d3d8011d9d5bec5367
anr4876 <dustjdanr@naver.com>
2023-08-09 17:09:43 +0900

    선택 화면

--- src/screens/menu1/selectionScreen.js
+++ src/screens/menu1/selectionScreen.js
@@ -7,6 +7,8 @@ import {
   View,
   TouchableOpacity,
   Platform,
+  ScrollView,
+  Image,
 } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
@@ -17,6 +19,7 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 
 const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 function HeaderLine({ navigate }) {
   const [postText, setPostText] = React.useState("");
@@ -64,29 +67,81 @@ function HeaderLine({ navigate }) {
 
 function MidLine({ navigate }) {
   return (
-    <View>
+    <View style={{ alignSelf: "flex-start", marginTop: -windowHeight * 0.32 }}>
       <FontAwesomeIcon icon={faLocationArrow} />
     </View>
   );
 }
 
-export default function SelectionScreen({ navigation }) {
+function BottomLine({ navigate }) {
+  let name = "온천 불가마 사우나";
+  let content = "대전광역시 유성구 봉명동 538-1";
   return (
-    <View style={styles.wrapper}>
+    <View
+      style={{
+        alignSelf: "flex-start",
+        marginTop: windowHeight * 0.35,
+        marginLeft: windowWidth * 0.07,
+      }}
+    >
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/test.png")}
+          style={styles.separateImage}
+        />
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.content}>{content}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Image
+          source={require("../../assets/test2.png")}
+          style={styles.separateImage}
+        />
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.content}>{content}</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function SelectionScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
       <HeaderLine navigate={navigation.navigate} />
-      <MidLine />
+      <ScrollView contentContainerStyle={{ flexGrow: 1, paddingTop: 64 }}>
+        <MidLine navigate={navigation.navigate} />
+        <BottomLine navigate={navigation.navigate} />
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  wrapper: {
+  container: {
     flex: 1,
+    backgroundColor: "#fff",
+    ...Platform.select({
+      ios: {
+        zIndex: 10,
+      },
+      android: {
+        elevation: 10,
+      },
+    }),
   },
   headerContainer: {
+    position: "absolute", // 여기에 position 속성 추가
+    top: 0, // 상단 위치를 0으로 설정
+    left: 0, // 왼쪽 위치를 0으로 설정
+    width: "100%", // 너비를 100%로 설정하여 전체 너비를 차지하게 합니다.
+    zIndex: 10, // 헤더가 항상 위에 올 수 있도록 zIndex 설정
+    paddingTop: 4,
+    paddingBottom: 4,
+
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: "white",
   },
   menuContainer: {
     flex: 1,
@@ -138,4 +193,37 @@ const styles = StyleSheet.create({
     marginRight: 8,
     fontSize: 10,
   },
+  name: {
+    width: windowWidth * 0.65,
+    height: windowHeight * 0.04,
+    left: windowWidth * 0.005,
+    marginTop: windowHeight * 0.01,
+    fontStyle: "normal",
+    fontWeight: "800",
+    fontSize: windowHeight * 0.02,
+    lineHeight: windowHeight * 0.027,
+    textAlign: "left",
+
+    color: "#000000",
+  },
+  content: {
+    width: windowWidth * 0.75,
+    height: windowHeight * 0.04,
+    left: windowWidth * 0.005,
+    fontStyle: "normal",
+    fontWeight: "500",
+    textAlign: "left",
+    fontSize: windowHeight * 0.015,
+    lineHeight: windowHeight * 0.02,
+    color: "#5E5E5E",
+  },
+  backgroundView: {
+    position: "absolute",
+    width: windowWidth * 0.8,
+    height: windowHeight * 0.3,
+    left: windowWidth * 0.05,
+    top: windowHeight * 0.35,
+  },
 });
+
+export default SelectionScreen;
--- src/screens/menu1Stack.js
+++ src/screens/menu1Stack.js
@@ -6,35 +6,15 @@ import DetailsScreen from "./menu1/detail";
 import HomeScreen from "./menu1/home";
 import ProfileScreen from "./menu1/profile";
 
-function LogoTitle() {
-  return (
-    <Image
-      style={{ width: 40, height: 40 }}
-      source={require(".././assets/test_rogo.png")}
-    />
-  );
-}
 const Stack = createNativeStackNavigator();
 
 export default function StackScreen1() {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        title: "My home",
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
+    <Stack.Navigator>
       <Stack.Screen
         name="Home"
         component={HomeScreen}
         options={{
-          headerTitle: (props) => <LogoTitle {...props} />,
           headerRight: () => (
             <Button
               onPress={() => alert("This is a button!")}
@@ -47,17 +27,17 @@ export default function StackScreen1() {
       <Stack.Screen
         name="Profile"
         component={ProfileScreen}
-        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Details"
         component={DetailsScreen}
-        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+        options={{ headerShown: false }}
       />
       <Stack.Screen
         name="Selection"
         component={SelectionScreen}
-        options={{ headerTitle: (props) => <LogoTitle {...props} /> }}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
