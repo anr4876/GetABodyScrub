9328c21d62e592b434d1b4a1e7f527d7a8c64239
Motoong <sonada903@gmail.com>
2023-08-11 15:32:45 +0900

    home.js에 gps추가

--- src/screens/menu1/home.js
+++ src/screens/menu1/home.js
@@ -1,9 +1,61 @@
 import * as React from "react";
 import { View, Text, Button } from "react-native";
+import * as Location from "expo-location"; //gps
+
 
 export default function HomeScreen({ navigation, route }) {
   const [count, setCount] = React.useState(0);
+  //gps
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+  const [cityName, setCityName] = React.useState("");
+  const [district, setDistrict] = React.useState("");
 
+  //gps useeffect
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let locationData = await Location.getCurrentPositionAsync({});
+      setLocation(locationData);
+
+      // Google Maps Geocoding API Key
+      const apiKey = 'AIzaSyDELVSja2BsgdKGMrrDkZfXe1MgRASGbtY';
+
+      // Get the city name and district using latitude and longitude
+      fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${locationData.coords.latitude},${locationData.coords.longitude}&key=${apiKey}&language=ko`)
+        .then((response) => response.json())
+        .then((data) => {
+          // Find the city and district in the API response data
+          for (let result of data.results) {
+            for (let component of result.address_components) {
+              if (component.types.includes("locality")) {
+                setCityName(component.long_name);
+              }
+
+              if (component.types.includes("sublocality_level_1")) {
+                setDistrict(component.long_name);
+              }
+              
+              if (cityName && district) {
+                break;
+              }
+            }
+          }
+
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+    })();
+  }, []);
+
+  //기존 코드
   React.useEffect(() => {
     // Use `setOptions` to update the button that we previously specified
     // Now the button includes an `onPress` handler to update the count
@@ -38,7 +90,7 @@ export default function HomeScreen({ navigation, route }) {
       <Button
         title="Selection Screen"
         onPress={() =>
-          navigation.navigate("Selection", { selectedDistrict: "서구" })
+          navigation.navigate("Selection", { selectedDistrict: district})
         }
       />
       <Button
@@ -49,6 +101,8 @@ export default function HomeScreen({ navigation, route }) {
       />
       <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
       <Text>Count: {count}</Text>
+      
+      <Text>현재지역 : {cityName} {district}</Text>
     </View>
   );
 }
